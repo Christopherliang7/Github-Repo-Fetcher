722e00b7bfcf9b30af29243fe594c5605691f505
Christopher <Christopherliang7@gmail.com>
2020-11-02 16:54:53 -0500

    Created MongoDB Schema

--- database/index.js
+++ database/index.js
@@ -1,16 +1,31 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fetcher');
 
+// Send yourself a notification if connection was sucessful or not
+// const db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'connection error:'));
+// db.once('open', function() {
+//   console.log('Successful Connection!')
+// });
+
+// once connection is created - create a schema to be derived by the model
 let repoSchema = mongoose.Schema({
-  // TODO: your schema here!
+  id: Number, // repo Id
+  repo_name: String,
+  owner_name: String,
+  owner_id: Number,
+  url: String,
+  forks: String
 });
 
+// once schema is created - compile schema into a model
 let Repo = mongoose.model('Repo', repoSchema);
 
 let save = (/* TODO */) => {
   // TODO: Your code here
   // This function should save a repo or repos to
   // the MongoDB
+
 }
 
 module.exports.save = save;
